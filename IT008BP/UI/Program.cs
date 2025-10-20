43ae224807c0568fb4dffc98bc30a2531b22f376
Thien Son <nhuson190605@gmail.com>
2025-10-20 22:12:59 +0700

    merge

--- IT008BP/UI/Program.cs
+++ IT008BP/UI/Program.cs
@@ -9,23 +9,12 @@ namespace UI
 {
     internal static class Program
     {
-<<<<<<< HEAD
-=======
-        /// <summary>
-        ///</summary>>
->>>>>>> ecaeab9e0b1810df1db193d8e56062d6405bb905
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new MenuForm());
-=======
-
-            // Mở form test DB
-            Application.Run(new TestDBForm());
->>>>>>> ecaeab9e0b1810df1db193d8e56062d6405bb905
         }
     }
 }
