6fdc8e0d676804a846aebc796ec8b93674643e3a
trucanh <24520085@gm.uit.edu.vn>
2026-01-10 22:50:03 +0700

    fix db

--- IT008BP/DATA/DataHelper.cs
+++ IT008BP/DATA/DataHelper.cs
@@ -1,52 +1,81 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Text.Json.Serialization;
-using System.Xml;
-namespace DATA
-{
-    public class DataHelper
+﻿    using System;
+    using System.Data.SQLite;
+    using System.IO;
+
+    namespace DATA
     {
-        private readonly string jsonFile;
-        private GameData data;
-
-        public class GameData
+        public class DataHelper
         {
-            public int Highscore { get; set; }
-        }
+            private readonly string dbFile;
+            private readonly string connectionString;
 
-        public DataHelper(string file)
-        {
-            jsonFile = file;
-
-            if (File.Exists(jsonFile))
+            public DataHelper(string file)
             {
-                var text = File.ReadAllText(jsonFile);
-                data = JsonConvert.DeserializeObject<GameData>(text) ?? new GameData { Highscore = 0 };
+                dbFile = file;
+                connectionString = $"Data Source={dbFile};Version=3;";
+
+                if (!File.Exists(dbFile))
+                {
+                    CreateDatabase();
+                }
             }
-            else
+
+            // Tạo DB + bảng
+            private void CreateDatabase()
             {
-                data = new GameData { Highscore = 0 };
-                Save();
+                SQLiteConnection.CreateFile(dbFile);
+
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql =
+                    @"CREATE TABLE GameData (
+                        Id INTEGER PRIMARY KEY,
+                        Highscore INTEGER NOT NULL
+                      );
+
+                      INSERT INTO GameData (Highscore) VALUES (0);";
+
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
             }
-        }
 
-        private void Save()
-        {
-            File.WriteAllText(jsonFile,
-                JsonConvert.SerializeObject(data, Newtonsoft.Json.Formatting.Indented)
-            );
-        }
-
-        public int GetHighscore() => data.Highscore;
-
-        public void UpdateHighscore(int score)
-        {
-            if (score > data.Highscore)
+            // Lấy highscore
+            public int GetHighscore()
             {
-                data.Highscore = score;
-                Save();
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT Highscore FROM GameData LIMIT 1;";
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        return Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+            }
+
+            // Cập nhật highscore
+            public void UpdateHighscore(int score)
+            {
+                int current = GetHighscore();
+                if (score <= current) return;
+
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql = "UPDATE GameData SET Highscore = @score;";
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@score", score);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
             }
         }
     }
-}
